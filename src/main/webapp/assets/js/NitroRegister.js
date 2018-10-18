8fa952de2a52b7c8697116d391a5d535a53bc8ef
Brandon Lugo <neobrandll@gmail.com>
2018-10-18 17:06:41 -0400

    adding files..

--- src/main/webapp/assets/js/NitroRegister.js
+++ src/main/webapp/assets/js/NitroRegister.js
@@ -1,17 +1,18 @@
-
-
+document.getElementById("registerbtn").addEventListener("click", function(){window.location.href ="Nitroregister.html"})
+document.getElementById("homebtn").addEventListener("click", function(){window.location.href ="index.html"})
+document.getElementById("loginbtn").addEventListener("click", function(){window.location.href ="Nitrologin.html"})
 
 document.getElementById("create").addEventListener("click", function(){
     var pwd = document.getElementById("pwd").value
     var rpwd = document.getElementById("rpwd").value
-    var email = document.getElementById("email").value
+    var mail = document.getElementById("email").value
     var user = document.getElementById("user").value
     var name = document.getElementById("create").value
     if(pwd === rpwd){
     var url = 'http://localhost:8080/NitroReader/Register';
     var data = {name: name,
-                pwd : pwd,
-                email: email,
+                password : pwd,
+                mail: mail,
                 user: user
                             };
 
@@ -21,10 +22,14 @@ document.getElementById("create").addEventListener("click", function(){
     headers:{
         'Content-Type': 'application/json'
     }
-    }).then(res => res.json())
+    }).then(res => res.json()).then(function(res){
+        if(res.status ==200 && validUser == true && validEmail == true && validPassword ==true && validName== true){
+            console.log("Register complete!")
+        }
+    })
     .catch(error => console.error('Error:', error))
     .then(response => console.log('Success:', response));
-        }else{alert("las contrasenas no coinciden!")}
+        }else{alert("passwords don't match!")}
 
     })
 
---
+++ src/main/webapp/assets/js/home.js
@@ -0,0 +1,3 @@
+document.getElementById("registerbtn").addEventListener("click", function(){window.location.href ="Nitroregister.html"})
+document.getElementById("homebtn").addEventListener("click", function(){window.location.href ="index.html"})
+document.getElementById("loginbtn").addEventListener("click", function(){window.location.href ="Nitrologin.html"})
