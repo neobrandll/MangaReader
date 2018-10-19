ce2c7f25baedc003702c7a169a34f0d845fea78f
Brandon Lugo <neobrandll@gmail.com>
2018-10-18 20:32:19 -0400

    updating...

--- src/main/webapp/assets/js/NitroRegister.js
+++ src/main/webapp/assets/js/NitroRegister.js
@@ -1,14 +1,18 @@
-document.getElementById("registerbtn").addEventListener("click", function(){window.location.href ="Nitroregister.html"})
+var loginbtn = document.getElementById("loginbtn")
+var registerbtn= document.getElementById("registerbtn")
+var logoutbtn = document.getElementById("logoutbtn")
+
+//los eventos para el redireccionado
+registerbtn.addEventListener("click", function(){window.location.href ="Nitroregister.html"})
 document.getElementById("homebtn").addEventListener("click", function(){window.location.href ="index.html"})
-document.getElementById("loginbtn").addEventListener("click", function(){window.location.href ="Nitrologin.html"})
-document.getElementById("relogin").addEventListener("click", function(){window.location.href ="Nitrologin.html"})
+document.getElementById("gohome").addEventListener("click", function(){window.location.href ="index.html"})
+loginbtn.addEventListener("click", function(){window.location.href ="Nitrologin.html"})
 
 document.getElementById("create").addEventListener("click", function(){
     var pwd = document.getElementById("pwd").value
     var rpwd = document.getElementById("rpwd").value
     var mail = document.getElementById("email").value
     var user = document.getElementById("user").value
-    var name = document.getElementById("create").value
     if(pwd === rpwd){
     var url = 'http://localhost:8080/NitroReader/Register';
     var data = {name: name,
@@ -29,12 +33,38 @@ document.getElementById("create").addEventListener("click", function(){
         }
     })
     .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
         }else{alert("passwords don't match!")}
 
     })
 
-
+    function main(){
+        if(localStorage.user){
+            var userspan= document.getElementById("userspan");
+            userspan.textContent = localStorage.user;
+            loginbtn.style.display = "none";
+            registerbtn.style.display= "none"
+        }else{
+            logoutbtn.style.display = "none"
+            
+        }
+    }
+    
+    logoutbtn.addEventListener("click", function(){
+        
+    
+        fetch('http://localhost:8080/NitroReader/SDestroy', {
+        method: 'GET', 
+        withCredentials: true,
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type': 'application/json'
+        }
+        }).then(res => res.json()).then(function(){
+           localStorage.clear();
+           window.location.replace("index.html");
+        })
+        .catch(error => console.error('Error:', error))
+    })
 
 
 
