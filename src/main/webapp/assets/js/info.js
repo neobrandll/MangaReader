06bd90b085be35cad35ae4b28615796d2c0a4933
Elvis Araujo <sivlej89@hotmail.com>
2018-11-06 22:56:56 -0400

    Editing Front of CRUDManga

--- src/main/webapp/assets/js/info.js
+++ src/main/webapp/assets/js/info.js
@@ -1,19 +1,37 @@
-function $(a){
-    return document.getElementById(a);
-}
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
 
 function load() {
-    fetch("/MangaServlet?manga_id=3", {method:'GET'}).then(res => res.json())
+    fetch(`http://localhost:8080/NitroReader/MangaServlet?manga=${getUrlParameter("manga")}`, {method:'GET'}).then(res => res.json())
     .then(res => {
-        $("mangaImage").src = res.data.direction;
-        $("mangaName").innerHTML = res.data.manga_name;
-        $("mangaSynopsis").innerHTML = res.data.manga_synopsis;
-        if (res.data.status) {
-            $("mangaStatus").classList.add("fas fa-check-circle");
-            $("mangaStatuss").innerHTML = "Ongoing"
+        document.getElementById("mangaImage").src = `http://localhost:8080/NitroReader/library/${res.data.location}`;
+        document.getElementById("mangaName").innerHTML = res.data.manga_name;
+        document.getElementById("mangaSynopsis").innerHTML = res.data.manga_synopsis;
+        console.log(res.data.manga_status);
+        if (res.data.manga_status) {
+            document.getElementById("mangaStatuss").innerHTML = "<i class='fas fa-check-circle'></i> Ongoing";
         } else{
-            $("mangaStatus").classList.add("fas fa-ban");
-            $("mangaStatuss").innerHTML = "Discontinued";
+            document.getElementById("mangaStatus").classList.add("fas fa-ban");
+            document.getElementById("mangaStatuss").innerHTML = "Discontinued";
         }
+        res.data.genres.forEach(element => {
+            let li = document.createElement("li");
+            li.setAttribute("class", "genre");
+            li.innerHTML = element;
+            document.getElementById("mangaGenres").appendChild(li);
+        });
+        
     })
 }
