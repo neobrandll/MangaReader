4347a583ae54a6e0a924bba7c8cf1a97fbaad245
Elvis Araujo <sivlej89@hotmail.com>
2018-11-12 21:03:50 -0400

    Comments Manga model

---
+++ src/main/java/models/CommentsManga.java
@@ -0,0 +1,31 @@
+package models;
+
+public class CommentsManga {
+    private String name;
+    private String comment;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
